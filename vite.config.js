541578336bb779e777cbee129d99e455913b113d
glorious73 <amjadmh73@gmail.com>
2024-10-13 21:12:38 +0300

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { resolve } from 'path';
+import handlebars from 'vite-plugin-handlebars';
+
+const pageContext = {
+    '/index.html': {
+      title: 'Main Page',
+    },
+    '/about.html': {
+      title: 'About Page',
+    },
+  };
+
+export default {
+  plugins: [
+    handlebars({
+        context(pagePath) {
+            return pageContext[pagePath]
+        },
+        partialDirectory: resolve(__dirname, 'partials')
+    })],
+};
