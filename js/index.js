af0870f4174c9a6d2cc76d723a0407d18670b548
glorious73 <amjadmh73@gmail.com>
2024-10-14 21:23:50 +0300

    header mobile friendly

--- js/index.js
+++ js/index.js
@@ -4,10 +4,14 @@ window.Globals = {
     isHeaderListShown: false
 };
 
+// TODO: position fixed and absolute for header on mobile
+// followed by flex column for footer
+// followed by first section of landing and dashboard screenshot
+// followed by features list
 const headerList = document.querySelector(".header-list");
 const headerListHandler = (e) => {
     Globals.isHeaderListShown = !Globals.isHeaderListShown;
-    document.querySelector(".header-links").className = (Globals.isHeaderListShown) ? "header-links show" : "header-links";
+    document.querySelector(".header-links").className = (Globals.isHeaderListShown) ? "header-links show" : "header-links hide";
 }
 
 headerList.addEventListener("click", headerListHandler);
--- js/styles.js
+++ js/styles.js
@@ -1,3 +1,4 @@
 import '../css/style.css';
 import '../css/header.css';
 import '../css/footer.css';
+import '../css/animation.css';
